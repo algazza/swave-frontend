2692e940aa615c815f8c12d064394d2ec41c0cad
algazza <fadhilaswadira72564@gmail.com>
2025-09-26 10:15:03 +0700

    ✨ feat: add header and footer

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { Dana, Gopay, logoWhite, QRIS, Spay } from "~/lib/image";
+
+const brandImg = [QRIS, Gopay, Dana, Spay];
+</script>
+
+<template>
+  <footer class="bg-foreground px-4 pt-10 text-background grid gap-5">
+    <div class="grid gap-2">
+      <div class="grid gap-4">
+        <img :src="logoWhite" alt="logo" class="w-[145px]" />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+          vel ipsum et arcu egestas pretium non nec eros. Class aptent taciti
+          sociosqu
+        </p>
+      </div>
+
+      <div class="flex justify-between">
+        <ul class="grid gap-2">
+          <span class="text-base font-semibold">Menu</span>
+          <li>Home</li>
+          <li>About</li>
+          <li>Shop</li>
+          <li>Account</li>
+        </ul>
+        <ul class="grid gap-2">
+          <span class="text-base font-semibold">Social</span>
+          <li>Instagram</li>
+          <li>Tiktok</li>
+          <li>Call Center</li>
+        </ul>
+        <ul class="grid gap-2">
+          <span class="text-base font-semibold">Payment</span>
+          <div
+            class="bg-background px-3 py-2 w-fit rounded-lg"
+            v-for="brand in brandImg"
+          >
+            <img :src="brand" :alt="brand" />
+          </div>
+        </ul>
+      </div>
+    </div>
+
+    <div class="border-t-[1px] border-background py-5 text-center">
+        &copy; All Right Reserved By <Link href="/" class="underline">Swave</Link> 
+    </div>
+  </footer>
+</template>
---
+++ app/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Menu, Moon, ShoppingCart, User } from "lucide-vue-next";
+import { logoBlack } from "~/lib/image";
+</script>
+
+<template>
+  <header class="fixed w-full p-5 top-0 left-0 bg-background z-[100]">
+    <nav class="flex justify-between items-center">
+      <div class="flex  items-center">
+        <Menu />
+        <img :src="logoBlack" alt="Swave" class="w-[131px]" />
+      </div>
+
+      <div class="flex gap-4 items-center">
+        <Moon />
+        <User />
+        <ShoppingCart />
+      </div>
+    </nav>
+  </header>
+</template>
