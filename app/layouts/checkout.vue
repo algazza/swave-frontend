f4427f90f2b08e4bbb7cc03d488bc0ab30aa69a2
algazza <fadhilaswadira72564@gmail.com>
2025-11-05 21:26:14 +0700

    ✨ feat: add checkout layout

---
+++ app/layouts/checkout.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { Moon, User } from "lucide-vue-next";
+import { logoBlack } from "~/lib/image";
+</script>
+
+<template>
+  <div>
+    <header
+      class="fixed w-full p-5 top-0 left-0 bg-background z-[100] md:px-10 lg:px-20"
+    >
+      <div class="w-full max-w-[1200px]">
+        <div class="flex justify-between">
+          <NuxtLink href="/">
+            <img :src="logoBlack" alt="Swave" class="w-[131px]" />
+          </NuxtLink>
+          <div class="flex gap-4 items-center justify-end">
+            <Moon />
+            <User />
+          </div>
+        </div>
+      </div>
+    </header>
+    <main
+      class="overflow-hidden mt-20 relative px-4 md:px-10 lg:px-20 grid justify-center"
+    >
+      <div class="max-w-[1200px]">
+        <slot />
+      </div>
+    </main>
+  </div>
+</template>
