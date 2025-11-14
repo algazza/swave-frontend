3db15aa51aba94077e3c8d725e43e65bd7693207
algazza <fadhilaswadira72564@gmail.com>
2025-11-14 09:43:05 +0700

    🏷️ feat: fixing type

--- app/types/product.ts
+++ app/types/product.ts
@@ -1,11 +1,34 @@
-import z from "zod"
+import z from "zod";
+
+const ProductVariants = z.object({
+  variant: z.string(),
+  stock: z.number(),
+  price: z.number(),
+});
+
+const ProductReview = z.object({
+  name: z.string(),
+  star: z.number(),
+  description: z.string(),
+});
 
 export const ProductSchema = z.object({
-    name: z.string(),
-    price: z.number(),
-    product_image: z.string().url(),
-    categories: z.string(),
-    review: z.number()
-})
+  name: z.string(),
+  price: z.number(),
+  product_image: z.string().url(),
+  categories: z.string(),
+  star: z.number(),
+});
 
-export type ProductType = z.infer<typeof ProductSchema>
+export const ProductDetailSchema = ProductSchema.extend({
+  description: z.string(),
+  product_image: z.array(z.string().url()),
+  stock: z.number(),
+  sold: z.number(),
+  variants: z.array(ProductVariants),
+  reviews: z.array(ProductReview),
+});
+
+
+export type ProductType = z.infer<typeof ProductSchema>;
+export type ProductDetailType = z.infer<typeof ProductDetailSchema>;
