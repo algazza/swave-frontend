ff144cf32f20a03ea9d1b4714eda8ee96f3384ea
algazza <fadhilaswadira72564@gmail.com>
2025-11-14 09:45:48 +0700

    🐛 fix: update layout styling

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,9 +5,9 @@
     <AppHeader />
 
     <main
-      class="overflow-hidden mt-20 relative px-4 md:px-10 lg:px-20 grid justify-center"
+      class="overflow-hidden mt-20 relative px-4 md:px-10 lg:px-20 flex justify-center"
     >
-      <div class="max-w-[1200px]">
+      <div class="max-w-[1200px] w-full">
         <slot />
     </div>
     </main>
