26076f3a9c8f629f9574f51dc10e8579c5d163cf
algazza <fadhilaswadira72564@gmail.com>
2025-11-18 13:59:13 +0700

    💄ui: add layout and sidebar for admin

---
+++ app/layouts/admin.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { SidebarInset, SidebarProvider } from "~/components/ui/sidebar";
+</script>
+
+<template>
+  <SidebarProvider
+  
+  >
+    <AdminSidebar />
+    <SidebarInset>
+      <main class="overflow-hidden">
+        <slot />
+      </main>
+    </SidebarInset>
+  </SidebarProvider>
+</template>
