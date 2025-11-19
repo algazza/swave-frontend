f4566320a0723635f13cb1609e30e8bd8b7876ed
algazza <fadhilaswadira72564@gmail.com>
2025-11-19 14:38:33 +0700

    ✨feat: add cart length notification in header

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -2,16 +2,18 @@
 import { Menu, Moon, ShoppingCart, User } from "lucide-vue-next";
 import { navLink } from "~/lib/constanta";
 import { logoBlack } from "~/lib/image";
+import { useCartStore } from "~/store/CartStore";
 import { useUiStore } from "~/store/UiStore";
 
-const ui = useUiStore()
+const ui = useUiStore();
+const cart = useCartStore()
 </script>
 
 <template>
   <header
     class="fixed w-full p-5 top-0 left-0 bg-background z-[100] md:px-10 lg:px-20 flex justify-center"
   >
-  <CartHeader/>
+    <CartHeader />
     <div class="w-full max-w-[1200px]">
       <nav class="flex justify-between items-center">
         <div class="flex items-center lg:hidden">
@@ -39,7 +41,13 @@ const ui = useUiStore()
         <div class="flex gap-4 items-center lg:w-[255px] justify-end">
           <Moon />
           <User />
-          <button @click="ui.toggleCart" class="cursor-pointer">
+          <button @click="ui.toggleCart" class="cursor-pointer relative">
+            <div
+            v-if="cart.cartLength > 0"
+              class="size-[22px] absolute -top-3 -right-3 bg-destructive text-background rounded-full text-xs flex justify-center items-center text-center font-bold"
+            >
+              {{ cart.cartLength > 99 ? '99+' : cart.cartLength}}
+            </div>
             <ShoppingCart />
           </button>
         </div>
