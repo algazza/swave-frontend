fa5c61e524d9cfbcedfa1099fc55db9185e8fb3c
algazza <fadhilaswadira72564@gmail.com>
2026-01-05 14:59:30 +0700

    ⚡️ perf: tightening the rules for filling out forms

--- app/composables/address/useEditAddress.ts
+++ app/composables/address/useEditAddress.ts
@@ -1,32 +1,32 @@
 import { push } from "notivue";
 import { useMutation, useQueryClient } from "@tanstack/vue-query";
 import type { AxiosError } from "axios";
+import type { EditAddressType } from "~/types/address";
 import type { ErrorResponse } from "~/types/error";
-import type { EditUserType } from "~/types/user";
 
-export const useEditUser = () => {
+export const useEditAddress = () => {
   const { $api } = useNuxtApp();
   const queryClient = useQueryClient();
-  
   return useMutation({
-    mutationFn: async (payload: EditUserType) => {
+    mutationFn: async (payload: EditAddressType & { id: number }) => {
       try {
         const token = useCookie("token");
-        const res = await $api.put("/account/profile", payload, {
+        const { id, ...addressData } = payload;
+        const res = await $api.put(`/account/address/${id}`, addressData, {
           headers: {
-            Authorization: token.value,
+            Authorization: `${token.value}`,
           },
         });
         return res.data;
       } catch (err) {
         const error = err as AxiosError<ErrorResponse>;
-        throw new Error(error.response?.data?.message || "Edit User gagal");
+        throw new Error(error.response?.data?.message || "Edit address gagal");
       }
     },
-    onSuccess() {
-      push.success("Success edit user");
+    onSuccess: () => {
+      push.success("Success Edit Address");
       queryClient.invalidateQueries({
-        queryKey: ["users"],
+        queryKey: ["address"],
         refetchType: "active",
       });
     },
