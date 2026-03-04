f1a1afd33dea5f47ec95b18aba4b15c7fdaa3d26
algazza <fadhilaswadira72564@gmail.com>
2026-03-04 09:26:41 +0700

    🔥perf: remove code and image  not used

--- app/lib/image.ts
+++ app/lib/image.ts
@@ -1,15 +1,12 @@
 import logoBlack from "/logo/swave_black.svg";
 import logoWhite from "/logo/swave_white.svg";
-import PickJagoan from "/pick/AYAM DEPAN.png";
-import PickHarmonight from "/pick/LUAR ANGKASA DEPAN.png";
-import PickDragon from "/pick/NAGA DEPAN.png";
-import PickStrawberry from "/pick/STROBERI DEPAN.png";
-import PickEpidemic from "/pick/TOPENG MERAH DEPAN.png";
-import NecklessDragon from "/neckless/Kalung Naga.png";
-import NecklessStrawberry from "/neckless/Kalung Stroberi.png";
-import NecklessEpidemic from "/neckless/Kalung Topeng.png";
-import ModelPick from "/neckless/Model Pick.png";
 import BlackPick from "/pick/blackPick.png"
+import AboutImg1 from "/AboutImage.png"
+import Aaf from "/personil/Aaf.png"
+import Cherish from "/personil/Cherish.png"
+import Mey from "/personil/Mey.png"
+import Regas from "/personil/Regas.png"
+import Rijal from "/personil/Rijal.png"
 import QRIS from "/brand/Qris.svg";
 import Gopay from "/brand/Gopay.svg";
 import Dana from "/brand/dana.svg";
@@ -18,16 +15,13 @@ import Spay from "/brand/Spay.svg";
 export {
   logoBlack,
   logoWhite,
-  PickDragon,
-  PickEpidemic,
-  PickHarmonight,
-  PickJagoan,
-  PickStrawberry,
-  NecklessDragon,
-  NecklessStrawberry,
-  NecklessEpidemic,
-  ModelPick,
   BlackPick,
+  AboutImg1,
+  Aaf,
+  Cherish,
+  Mey,
+  Regas,
+  Rijal,
   QRIS,
   Gopay,
   Dana,
